a7ed89d3c89518dd9cee88ba2a2e57c01c39f5a4
harshamin <hamin04@students.cs.ubc.ca>
2024-08-11 17:07:38 -0600

    REST API intial routes configuration

--- server/server.js
+++ server/server.js
@@ -1,8 +1,32 @@
-import express from "express";
+//import { config } from "dotenv";
+//import express from "express";
+require("dotenv").config();
+const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log('server running on port ' + port);
 });
+
+app.get("/api/v1/restaurants", (req,res) => {
+    //Return list of restaurants from postgres
+});
+
+app.get("/api/v1/restaurants/:id", (req,res) => {
+    //Return single restaurant from postgres using id
+});
+
+app.post("/api/v1/restaurants", (req,res) => {
+    //Create a restaurant
+});
+
+app.put("/api/v1/restaurants/:id", (req,res) => {
+    //Update a restaurant
+});
+
+app.delete("/api/v1/restaurants/:id", (req,res) => {
+    //Delete a restaurant
+});
+
