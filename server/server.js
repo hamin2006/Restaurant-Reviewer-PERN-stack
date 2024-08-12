6aed6b7072b0895975a2d4b05e773cba91e7d972
harshamin <hamin04@students.cs.ubc.ca>
2024-08-11 19:06:43 -0600

    api Get route configured

--- server/server.js
+++ server/server.js
@@ -1,17 +1,32 @@
-//import { config } from "dotenv";
-//import express from "express";
 require("dotenv").config();
-const express = require("express");
 
+const express = require("express");
+const db = require("./db");
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(express.json());
 
 app.listen(port, () => {
     console.log('server running on port ' + port);
 });
 
-app.get("/api/v1/restaurants", (req,res) => {
+app.get("/api/v1/restaurants", async (req,res) => {
     //Return list of restaurants from postgres
+    try {
+        const results = await db.query("select * from restaurants");
+        res.status(200).json({
+            status: "success",
+            results: results.rows.length,
+            data: {
+                restaurants: results.rows
+            },
+        });
+    } catch (e) {
+        console.log(e);
+    }
+    
+
 });
 
 app.get("/api/v1/restaurants/:id", (req,res) => {
